b98052bbc367f5a38cb335493692b8dc06e9e143
Bogdan Martyshevskiy <vigoroous@gmail.com>
2023-06-22 14:05:00 +0300

    Fix: convert input values to string;

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { NUpload, NUploadDragger, NP, NText, NIcon, NButton, NProgress } from "naive-ui"
-import { ArchiveOutline } from '@vicons/ionicons5'
-import type { UploadInst, UploadFileInfo } from 'naive-ui'
+import {
+  NUpload,
+  NUploadDragger,
+  NP,
+  NText,
+  NIcon,
+  NButton,
+  NProgress,
+} from "naive-ui";
+import { ArchiveOutline } from "@vicons/ionicons5";
+import type { UploadInst, UploadFileInfo } from "naive-ui";
 import { createReportXslx, readColumnXlsx } from "@/utils/workbook.utils";
 import { first } from "lodash";
 import { postSuggestions } from "@/api/dadata/postSuggestions";
@@ -16,7 +24,7 @@ type CompanyRecord = {
   status: string;
   liquidationDate?: string;
   annotation: string;
-}
+};
 
 const fileList = ref<UploadFileInfo[]>([]);
 const upload = ref<UploadInst | null>(null);
@@ -25,8 +33,7 @@ const progress = ref(0);
 
 const handleChange = (data: { fileList: UploadFileInfo[] }) => {
   fileList.value = data.fileList;
-}
-
+};
 
 // TODO: Immutable js Map (with complex key)
 const handleClick = async () => {
@@ -37,13 +44,14 @@ const handleClick = async () => {
   if (!file) return;
 
   const buffer = await file.arrayBuffer();
-  const innArray = readColumnXlsx(buffer)
-    .flatMap(line => line.split(/[ ;]/g));
-  // progress.value = 10;
+
+  const innArray = readColumnXlsx(buffer).flatMap((line: string | number) => {
+    return line.toString().split(/[ ;]/g);
+  });
+  progress.value = 10;
 
   const errArray: string[] = [];
   const rows: CompanyRecord[] = [];
-
 
   for (const [key, inn] of innArray.entries()) {
     progress.value = Math.round((key / innArray.length) * 100);
@@ -69,8 +77,9 @@ const handleClick = async () => {
       }
 
       for (const suggestion of res.suggestions) {
-        if ((suggestion.data.type === "LEGAL" &&
-          suggestion.data.branch_type === "MAIN") ||
+        if (
+          (suggestion.data.type === "LEGAL" &&
+            suggestion.data.branch_type === "MAIN") ||
           suggestion.data.type === "INDIVIDUAL"
         ) {
           rows.push({
@@ -78,14 +87,14 @@ const handleClick = async () => {
             inn: suggestion.data.inn,
             ogrn: suggestion.data.ogrn,
             status: OrgStatusDescription[suggestion.data.state.status],
-            liquidationDate: (suggestion.data.state.liquidation_date ?
-              new Date(suggestion.data.state.liquidation_date) :
-              null)?.toLocaleDateString("ru-RU"),
+            liquidationDate: (suggestion.data.state.liquidation_date
+              ? new Date(suggestion.data.state.liquidation_date)
+              : null
+            )?.toLocaleDateString("ru-RU"),
             annotation: "",
           });
         }
       }
-
     } else {
       errArray.push(inn);
     }
@@ -94,13 +103,18 @@ const handleClick = async () => {
   await createReportXslx(rows, errArray);
 
   loading.value = false;
-}
-
-
+};
 </script>
 
 <template>
-  <NUpload ref="upload" directory-dnd accept=".xlsx" :default-upload="false" @change="handleChange" :max="1">
+  <NUpload
+    ref="upload"
+    directory-dnd
+    accept=".xlsx"
+    :default-upload="false"
+    @change="handleChange"
+    :max="1"
+  >
     <NUploadDragger>
       <div style="margin-bottom: 12px">
         <NIcon size="48" :depth="3">
@@ -116,12 +130,24 @@ const handleClick = async () => {
       </NP>
     </NUploadDragger>
   </NUpload>
-  <NButton :disabled="!fileList.length" style="margin-bottom: 12px" @click="handleClick" :loading="loading">
+  <NButton
+    :disabled="!fileList.length"
+    style="margin-bottom: 12px"
+    @click="handleClick"
+    :loading="loading"
+  >
     Получить отчет
   </NButton>
 
-  <NProgress v-if="loading" type="line" :height="24" :border-radius="4" :percentage="progress"
-    :indicator-placement="'inside'" processing />
+  <NProgress
+    v-if="loading"
+    type="line"
+    :height="24"
+    :border-radius="4"
+    :percentage="progress"
+    :indicator-placement="'inside'"
+    processing
+  />
 </template>
 
 <style scoped></style>
